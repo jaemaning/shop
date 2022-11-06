b58788eec751a9b29b8dcfcb6ede4e38b81d6511
김재만 <lovesay00@naver.com>
2022-11-07 04:03:28 +0900

    20221107_kjm(nested route)

--- src/App.js
+++ src/App.js
@@ -2,23 +2,24 @@ import './App.css';
 import { Navbar, Container, Nav, Row, Col } from 'react-bootstrap';
 import data from './data.js';
 import { useState } from 'react';
-import { Routes, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import Detail from './routes/Detail.js'
 
 function App() {
 
-  let [shoes] = useState(data)
+  let [shoes] = useState(data);
+  let navigate = useNavigate();
 
   return (
     <div className="App">
 
       <Navbar bg="dark" variant="dark">
         <Container>
-          <Navbar.Brand href="#home">Jaem</Navbar.Brand>
+          <Navbar.Brand onClick={() => { navigate('/') }}>Jaem</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/') }}>Home</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/about') }}>Features</Nav.Link>
+            <Nav.Link onClick={() => { navigate('/detail') }}>Detail</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
@@ -42,7 +43,15 @@ function App() {
           </>
         } />
         <Route path='/detail' element={<Detail />} />
-        <Route path='/about' element={<div>about 페이지임</div>} />
+        <Route path='/about' element={<About />}>
+          <Route path='member' element={<div>멤버임</div>} />
+          <Route path='location' element={<div>위치정보임</div>} />
+        </Route>
+        <Route path='/event' element={<Eventpage />}>
+          <Route path='one' element={<div>첫 주문시 양배추즙 서비스</div>} />
+          <Route path='two' element={<div>생일기념 쿠폰받기</div>} />
+        </Route>
+        <Route path='/all' element={<div>404 page error</div>} />
       </Routes>
 
     </div >
@@ -57,6 +66,24 @@ function Card(props) {
       <p>{props.shoes.content}</p>
       <p>{props.shoes.price} 원</p>
     </Col>
+  )
+}
+
+function About() {
+  return (
+    <div>
+      <h4>회사정보임</h4>
+      <Outlet></Outlet>
+    </div>
+  )
+}
+
+function Eventpage() {
+  return (
+    <div>
+      <h1>오늘의 이벤트</h1>
+      <Outlet></Outlet>
+    </div>
   )
 }
 
