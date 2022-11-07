d91b6e66f814cea2f0499e905ae64dd603fa053b
김재만 <lovesay00@naver.com>
2022-11-07 22:08:05 +0900

    20221107_kjm(URL parameter)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,18 +1,25 @@
-function Detail() {
+import { useParams } from "react-router-dom"
+
+function Detail(props) {
+
+    let { id } = useParams();
+    let dataid = props.shoes[id].id
+    let picnum = Number(dataid) + 1;
+
     return (
         <div className="container">
             <div className="row">
-                <div className="col-md-6">
-                    <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%" />
+                <div className="pos-center">
+                    <img src={"https://codingapple1.github.io/shop/shoes" + picnum + ".jpg"} width="100%" />
                 </div>
-                <div className="col-md-6">
-                    <h4 className="pt-5">상품명</h4>
-                    <p>상품설명</p>
-                    <p>120000원</p>
+                <div className="col-md-12 mt-4">
+                    <h4 className="pt-5">{props.shoes[dataid].title}</h4>
+                    <p>{props.shoes[dataid].content}</p>
+                    <p>{props.shoes[dataid].price} 원</p>
                     <button className="btn btn-danger">주문하기</button>
                 </div>
             </div>
-        </div>
+        </div >
     )
 }
 
