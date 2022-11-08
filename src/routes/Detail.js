d4fd0189402918d18a80ec92395d384933a437db
김재만 <lovesay00@naver.com>
2022-11-09 02:47:53 +0900

    20221109_kjm(useEffect)

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom"
-import styled from 'styled-components'
 
-let Btn = styled.button`
-  background : ${props => props.bg};
-  color : black;
-  padding : 10px;
-`
+
 
 function Detail(props) {
+
+    let [alertdiv, setAlertdiv] = useState(true)
+    let [alertbox, setAlertbox] = useState(false)
+
+    useEffect(() => {
+        let a = setTimeout(() => { setAlertdiv(false) }, 2000)
+
+        return () => {
+            clearTimeout(a)
+        }
+    }, [])
+
+    let onChange = (e) => {
+        console.log(Number(e.target.value))
+        let ab = Number(e.target.value)
+        if (isNaN(ab) == true) {
+            alert('숫자만 입력하세요')
+            setAlertbox(true)
+        }
+    }
+
+
 
     let { id } = useParams();
     let dataid = props.shoes[id].id
@@ -15,12 +33,24 @@ function Detail(props) {
 
     return (
         <div className="container">
-            <Btn bg='yellow'>버튼</Btn>
+            {
+                alertdiv == true ?
+                    <div className="alert alert-warning">
+                        2초 이내 구매시 할인
+                    </div>
+                    : null
+            }
             <div className="row">
                 <div className="pos-center">
                     <img src={"https://codingapple1.github.io/shop/shoes" + picnum + ".jpg"} width="100%" />
                 </div>
+                {
+                    alertbox == true
+                        ? <div className="alertRedbox">경고 : 숫자만 입력하세요.</div>
+                        : null
+                }
                 <div className="col-md-12 mt-4">
+                    <input className="inputbox" placeholder="숫자만 입력하세요" onChange={onChange}></input>
                     <h4 className="pt-5">{props.shoes[dataid].title}</h4>
                     <p>{props.shoes[dataid].content}</p>
                     <p>{props.shoes[dataid].price} 원</p>
